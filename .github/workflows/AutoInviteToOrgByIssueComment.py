2e60465f79fb9f902439b14f728d25467bc91d22
John Hurst <56172436+gwutrexl@users.noreply.github.com>
2021-09-28 08:22:07 -0400

    uncomment output for debugging

--- .github/workflows/AutoInviteToOrgByIssueComment.py
+++ .github/workflows/AutoInviteToOrgByIssueComment.py
@@ -14,10 +14,10 @@ import requests
 # else:
 #     print('Maybe running locally?')
 
-# print("Environ:")
-# print(os.environ)
-# print("Prefix:")
-# print(sys.prefix)
+print("Environ:")
+print(os.environ)
+print("Prefix:")
+print(sys.prefix)
 
 MY_GITHUB_KEY = os.environ['MY_GITHUB_KEY']
 COMMUNITY_TEAM_ID = os.environ['COMMUNITY_TEAM_ID']
@@ -25,8 +25,8 @@ COMMUNITY_TEAM_ID = os.environ['COMMUNITY_TEAM_ID']
 file = open(os.environ['GITHUB_EVENT_PATH'])
 data = json.load(file)
 
-# print("Data:")
-# print(data)
+print("Data:")
+print(data)
 
 COMMENT = data["comment"]["body"]
 USERNAME = data["comment"]["user"]["login"]
